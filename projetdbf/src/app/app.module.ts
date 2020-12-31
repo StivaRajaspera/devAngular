0ba044349d6c019dcd317d3cfb03c0f7b7813eeb
Asma972 <49292965+Asma972@users.noreply.github.com>
2020-12-31 13:24:02 +0100

    adding header and footer

--- projetdbf/src/app/app.module.ts
+++ projetdbf/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SignUpComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
