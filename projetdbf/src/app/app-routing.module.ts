a5573d57f2bbc2ad878b809be8b981a6d1575eed
Asma972 <49292965+Asma972@users.noreply.github.com>
2020-12-31 13:47:22 +0100

    corrections de quelques errreurs(header)

--- projetdbf/src/app/app-routing.module.ts
+++ projetdbf/src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 const routes: Routes = [
   { path: 'signup', component: SignUpComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: '', component: LoginComponent }
 ];
 
 @NgModule({
